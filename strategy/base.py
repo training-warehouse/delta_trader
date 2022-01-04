a987c39c45733a04ae28f8f40b7150d6d82d9eca
liaokongVFX <568250549@qq.com>
2022-01-04 23:07:47 +0800

    完成夏普率计算

--- strategy/base.py
+++ strategy/base.py
@@ -63,43 +63,4 @@ def calculate_sharpe(data):
 
     sharpe = avg_return / sd_return
     sharpe_year = sharpe * np.sqrt(252)
-    return sharpe,sharpe_year
-
-
-def week_period_strategy(code, frequency, start_date, end_date):
-    """周期选股（周四买，周一卖）"""
-    data = st.get_single_price(code, frequency, start_date, end_date)
-    data['weekday'] = data.index.weekday
-    # 周四买入
-    data['buy_signal'] = np.where((data['weekday'] == 3), 1, 0)
-    # 周一卖出
-    data['sell_signal'] = np.where((data['weekday'] == 0), -1, 0)
-
-    data = compose_signal(data)
-    data = calculate_prof_pct(data)
-    data = calculate_cum_prof(data)
-    data = calculate_max_drawdown(data)
-
-    return data
-
-
-if __name__ == '__main__':
-    # df = week_period_strategy('000001.XSHE', 'daily', None,
-    #                           datetime.date.today())
-    # print(df[['close', 'signal', 'profit_pct', 'cum_prof']])
-    # print(df.describe())
-    # df['cum_prof'].plot()
-    # plt.show()
-
-    # df = st.get_single_price('000001.XSHE', 'daily', '2006-01-01',
-    #                          '2021-01-01')
-    # df = calculate_max_drawdown(df)
-    # print(df[['close', 'roll_max', 'daily_dd', 'max_dd']])
-    # df[['daily_dd', 'max_dd']].plot()
-    # plt.show()
-
-    # 计算夏普比率
-    df = st.get_single_price('000001.XSHE', 'daily', '2006-01-01',
-                             '2021-01-01')
-    df = calculate_sharpe(df)
-    print(df)
+    return sharpe, sharpe_year
