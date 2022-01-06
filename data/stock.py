6d412888e3a4f3a14bed8f8b336a241d759d48f1
liaokongVFX <568250549@qq.com>
2022-01-06 23:54:49 +0800

    完成数据下载和读取函数

--- data/stock.py
+++ data/stock.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import datetime
 
 import jqdatasdk
 import pandas as pd
@@ -14,6 +15,13 @@ pd.set_option('display.max_columns', 1000)
 file_root = os.path.dirname(__file__)
 
 
+def init_db():
+    stocks = get_stocks()
+    for stock in stocks:
+        data = get_single_price(code=stock, frequency='daily')
+        export_data(data, 'price', stock)
+
+
 def get_stocks():
     """
      获取所有A股股票列表
@@ -22,7 +30,7 @@ def get_stocks():
     return list(jqdatasdk.get_all_securities(['stock']).index)
 
 
-def get_single_price(code, frequency, start_date, end_date):
+def get_single_price(code, frequency, start_date=None, end_date=None):
     """
     获取单个股票行情数据
     :param code:
@@ -35,11 +43,14 @@ def get_single_price(code, frequency, start_date, end_date):
     if start_date is None:
         start_date = jqdatasdk.get_security_info(code).start_date
 
+    if end_date is None:
+        end_date = datetime.datetime.today()
+
     return jqdatasdk.get_price(code, start_date=start_date, end_date=end_date,
                                frequency=frequency)
 
 
-def export_data(data, data_type, filename):
+def export_data(data, data_type, filename, mode='w'):
     """
     导出数据
     :param data:
@@ -49,13 +60,23 @@ def export_data(data, data_type, filename):
     """
     file_path = f'{file_root}/{data_type}/{filename}.csv'
     data.index.names = ['date']
-    data.to_csv(file_path)
+    if mode == 'a':
+        data.to_csv(file_path, mode=mode, header=False)
+        data = pd.read_csv(file_path)
+        data = data.drop_duplicates('date')  # 针对日期去重，保留最后一个参数
+        data.to_csv(file_path, index=False)
+    else:
+        data.to_csv(file_path)
     print(f'已成功存储到： {file_path}')
 
 
-def get_csv_data(data_type, filename):
-    file_path = f'{file_root}/{data_type}/{filename}.csv'
-    return pd.read_csv(file_path)
+def get_csv_data(data_type, code, start_date, end_date):
+    """获取本地数据"""
+    update_daily_price(code, data_type)
+    file_path = f'{file_root}/{data_type}/{code}.csv'
+    data = pd.read_csv(file_path, index_col='date')
+
+    return data[(data.index >= start_date) & (data.index <= end_date)]
 
 
 def transfer_price_freq(data, time_freq):
@@ -95,3 +116,15 @@ def calculate_change_pct(data):
     data['close_pct'] = (data['close'] - data['close'].shift(1)) / data[
         'close'].shift(1)
     return data
+
+
+def update_daily_price(stock_code, data_type):
+    file_path = f'{file_root}/{data_type}/{stock_code}.csv'
+    if os.path.exists(file_path):
+        start_date = pd.read_csv(file_path, usecols=['date'])['date'].iloc[-1]
+        data = get_single_price(stock_code, 'daily', start_date,
+                                datetime.datetime.today())
+        export_data(data, data_type, stock_code, 'a')
+    else:
+        data = get_single_price(stock_code, 'daily')
+        export_data(data, data_type, stock_code)
